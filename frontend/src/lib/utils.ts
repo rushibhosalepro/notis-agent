415de9abf1f18561f84305656aacd5918099a344
Rushikesh <rushibhosalepro@gmail.com>
2026-06-10 08:43:54 +0530

    chores - change server url

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,5 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const SERVER_URL = process.env.NEXT_PUBLIC_SEVER_URL;
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
 export const userId = "temp-user-123";
